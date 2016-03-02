5a6565925d8f843420d918bbe8ca3593fcf8a65b
mimrock <lados.denes@gmail.com>
2016-03-02 13:58:57 +0100

    Refactor as a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'content-hub-python',
+  packages = ['content-hub-python'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python client library for Acquia Content Hub',
+  author = 'Acquia',
+  author_email = 'denes.lados@acquia.com',
+  url = 'https://github.com/acquia/content-hub-python', # use the URL to the github repo
+  download_url = 'https://github.com/acquia/content-hub-python/tarball/0.1', # I'll explain this in a second
+  keywords = ['content-hub', 'acquia'], # arbitrary keywords
+  classifiers = [],
+)
